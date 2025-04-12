46c3b08fe6636f1552356fb93906ecfea62efb8e
Maria Leitão <88951059+marialuizaleitao@users.noreply.github.com>
2025-04-12 15:15:01 -0300

    refactor: app routes to new auth route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import CreateAccount from './pages/CreateAccount';
-import Login from './pages/Login';
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Auth from './pages/Auth';
 import EditarUsuario from './pages/EditarUsuario';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<CreateAccount />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/auth" element={<Auth />} />
         <Route path="/editar" element={<EditarUsuario />} />
-        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route path="/" element={<Navigate replace to="/auth" />} />
       </Routes>
     </Router>
   );
