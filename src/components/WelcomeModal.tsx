2165672a991ae3fc5532b74009f1dae4549b929f
Victoria Fernandes <victoriafernandesgalvao@hotmail.com>
2025-04-12 14:39:23 -0300

    fix: z-index

--- src/components/WelcomeModal.tsx
+++ src/components/WelcomeModal.tsx
@@ -26,7 +26,7 @@ const WelcomeModal: React.FC<WelcomeModalProps> = ({ onClose }) => {
   ];
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[9999]  p-4">
       <div className="bg-white rounded-xl max-w-3xl w-full relative">
         {/* Close button */}
         <button
