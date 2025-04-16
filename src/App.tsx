74fcde49f64d642192e5cd52d05286a75f27a246
Maria Leitão <88951059+marialuizaleitao@users.noreply.github.com>
2025-04-16 15:51:43 -0300

    refactor: app file to config toast settings

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Auth from './pages/Auth';
 import EditarUsuario from './pages/EditarUsuario';
+import { ToastContainer } from 'react-toastify';
 
 const App: React.FC = () => {
   return (
@@ -11,6 +12,19 @@ const App: React.FC = () => {
         <Route path="/editar" element={<EditarUsuario />} />
         <Route path="/" element={<Navigate replace to="/auth" />} />
       </Routes>
+
+      {/* Toast configuration */}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </Router>
   );
 };
