f8a7a502c5371f9a72d8f70a1fa29bfc78b8462c
Victoria Fernandes <victoriafernandesgalvao@hotmail.com>
2025-05-03 18:22:33 -0300

    feat: add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import Auth from './pages/Auth';
-import BeachDetails from './pages/BeachDetails';
-import EditUser from './pages/EditUser';
-import Home from './pages/Home';
+import React from "react";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Auth from "./pages/Auth";
+import BeachDetails from "./pages/BeachDetails";
+import EditUser from "./pages/EditUser";
+import Home from "./pages/Home";
+import EvaluationFeed from "./pages/EvaluationFeed";
+import PhotoFeed from "./pages/PhotoFeed";
 
 const App: React.FC = () => {
   return (
@@ -15,6 +22,8 @@ const App: React.FC = () => {
         <Route path="/" element={<Navigate replace to="/auth" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/praia" element={<BeachDetails />} />
+        <Route path="/feedfotos" element={<PhotoFeed />} />
+        <Route path="/avaliacoes" element={<EvaluationFeed />} />
       </Routes>
 
       {/* Toast configuration */}
