daaf6c7a85a40e261b0edb9cf9e1cbeeec7e326f
Maria Leitão <88951059+marialuizaleitao@users.noreply.github.com>
2025-05-12 09:36:59 -0300

    feature: user router

---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/authContext";
+
+export const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
+  const { user } = useAuth();
+
+  return user ? <>{children}</> : <Navigate to="/auth" replace />;
+};
