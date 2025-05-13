7e121f9bdee4eb25f8ad1a829e899bfddbe63e5f
Maria Leitão <88951059+marialuizaleitao@users.noreply.github.com>
2025-05-13 09:48:03 -0300

    HOTFIX: token

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,7 +12,7 @@ const apiClient = axios.create({
 
 apiClient.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('flop-auth-token')
+    const token = localStorage.getItem('authToken')
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
