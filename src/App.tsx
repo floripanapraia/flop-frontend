e49dd648c157db2e8cf637ee47988178d99707cd
Maria Leitão <88951059+marialuizaleitao@users.noreply.github.com>
2025-05-31 14:45:17 -0300

    refactor: app to handle new provider logic

--- src/App.tsx
+++ src/App.tsx
@@ -6,80 +6,108 @@ import {
   Routes,
 } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import { PrivateRoute } from "./routes/PrivateRoute";
-import { AdminRoute } from "./routes/AdminRoute";
 
 import Auth from "./pages/Auth";
-import Home from "./pages/Home";
 import EditUser from "./pages/EditUser";
-import FlopFeed from "./pages/FlopFeed";
-import PhotoFeed from "./pages/PhotoFeed";
 import EvaluationFeed from "./pages/EvaluationFeed";
-import { AuthProvider } from "./context/authContext";
-// import AdminUsers from "./pages/admin/AdminUsers"; // exemplo de rota admin
+import FlopFeed from "./pages/FlopFeed";
+import Home from "./pages/Home";
+import PhotoFeed from "./pages/PhotoFeed";
+
+import { AuthProvider } from "./contexts/authContext";
+import { UserProvider } from "./contexts/userContext";
+import ProtectedRoute from "./routes/ProtectedRoute";
+
+// import AdminUsers from "./pages/admin/AdminUsers"; // exemplo de rota admin só pra fazer funcionar
+function AdminUsers() {
+  return <div>Admin Users</div>;
+}
 
 const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/auth" />} />
-          <Route path="/auth" element={<Auth />} />
+    <Router>
+      <AuthProvider>
+        <UserProvider>
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/auth" element={<Auth />} />
 
-          <Route
-            path="/home"
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/editar"
-            element={
-              <PrivateRoute>
-                <EditUser />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/flops"
-            element={
-              <PrivateRoute>
-                <FlopFeed />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/fotos"
-            element={
-              <PrivateRoute>
-                <PhotoFeed />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/avaliacoes"
-            element={
-              <PrivateRoute>
-                <EvaluationFeed />
-              </PrivateRoute>
-            }
-          />
-          {/* <Route
-            path="/admin/users"
-            element={
-              <AdminRoute>
-                <AdminUsers />
-              </AdminRoute>
-            }
-          /> */}
-        </Routes>
+            {/* Redirect root to auth */}
+            <Route path="/" element={<Navigate to="/auth" replace />} />
 
-        <ToastContainer position="top-right" autoClose={5000} />
-      </Router>
-    </AuthProvider>
+            {/* Private Routes */}
+            <Route
+              path="/home"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/editar"
+              element={
+                <ProtectedRoute>
+                  <EditUser />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/flop-feed"
+              element={
+                <ProtectedRoute>
+                  <FlopFeed />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/photo-feed"
+              element={
+                <ProtectedRoute>
+                  <PhotoFeed />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/evaluation-feed"
+              element={
+                <ProtectedRoute>
+                  <EvaluationFeed />
+                </ProtectedRoute>
+              }
+            />
+
+            {/* Admin Routes */}
+            <Route
+              path="/admin/users"
+              element={
+                <ProtectedRoute adminOnly>
+                  <AdminUsers />
+                </ProtectedRoute>
+              }
+            />
+
+            {/* Catch all route - redirect to auth */}
+            <Route path="*" element={<Navigate to="/auth" replace />} />
+          </Routes>
+
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+        </UserProvider>
+      </AuthProvider>
+    </Router>
   );
 };
 
